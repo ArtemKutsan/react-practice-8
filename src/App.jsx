f915405c43e9021f1f8f71d3e1d31bc489c0093e
Artem Kutsan <artemkutsan@gmail.com>
2026-02-18 13:15:03 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        <h1 className="container">React Practice 8</h1>
+
+        <section>
+          <div className="container">
+            <h2>Задача 1: Бронирование билетов</h2>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
