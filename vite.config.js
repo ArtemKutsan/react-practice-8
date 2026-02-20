6247aca7e2b6a44c8a219f61a62b2b6da2146719
Artem Kutsan <artemkutsan@gmail.com>
2026-02-20 18:55:45 +0100

    add: Добавлены настройки для профилирования React

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'react-dom/client': 'react-dom/profiling',
+      'react-dom$': 'react-dom/profiling',
+      'scheduler/tracing': 'scheduler/tracing-profiling',
+    },
+  },
+});
