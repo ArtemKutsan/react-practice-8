75c30d84a5e3c08e1e402c253d9812be4ea26205
Artem Kutsan <artemkutsan@gmail.com>
2026-02-20 19:07:57 +0100

    add: Добавлена логика состояния для выбранного дня из списка

--- src/components/EventBooking/index.jsx
+++ src/components/EventBooking/index.jsx
@@ -2,14 +2,30 @@
 import styles from './EventBooking.module.css';
 import { useContext, useState } from 'react';
 import EventsContext from '../../context/events-context';
+import EventDetails from '../EventDetails';
 
 const EventBooking = () => {
   const { data } = useContext(EventsContext);
 
+  const [dayId, setDayId] = useState(null);
+  const [eventId, setEventId] = useState(null);
+
+  const eventsDay = data.find((day) => day.id === dayId);
+  const event = eventsDay?.events.find((event) => event.id === eventId);
+
+  const handleDateChange = (e) => {
+    setDayId(Number(e.target.value));
+    setEventId(null);
+  };
+
+  const handleEventChange = (e) => {
+    setEventId(Number(e.target.value));
+  };
+
   return (
     <div style={{ backgroundColor: '#eee', padding: '2rem', borderRadius: '1rem' }}>
       <form style={{ display: 'flex', gap: '1rem' }}>
-        <select className={styles.select}>
+        <select onChange={handleDateChange} value={dayId ?? ''} className={styles.select}>
           <option value="" disabled hidden>
             Выберите дату
           </option>
@@ -20,6 +36,20 @@ const EventBooking = () => {
             </option>
           ))}
         </select>
+
+        {eventsDay && (
+          <select onChange={handleEventChange} value={eventId ?? ''} className={styles.select}>
+            <option value="" disabled hidden>
+              Выберите событие
+            </option>
+
+            {eventsDay.events.map((event) => (
+              <option key={event.id} value={event.id}>
+                {event.title}
+              </option>
+            ))}
+          </select>
+        )}
       </form>
     </div>
   );
